dc3ede820fa1db8a99aac3375a397d4933375352
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-07-05 06:19:27 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -47,10 +47,45 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter_by(id=user_id).first()
+            if user:
+                return user.to_dict(), 200
+        return {}, 401
+
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        if 'username' not in data:
+            return {'error': 'Username not provided'}, 400
+
+        username = data['username']
+        user = User.query.filter_by(username=username).first()
+        
+        if not user:
+            return {'error': 'User not found'}, 404
+
+        session['user_id'] = user.id
+
+        return user.to_dict(), 200
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Logout, '/logout')
+api.add_resource(Login, '/login')
 
 
 if __name__ == '__main__':
